ca55a49c226e652bd86319661a86c89e663c1cc5
Johan van Dijk <pa3ang  at amsat.org>
2014-11-05 00:12:18 +0100

    Latest version with bugfixes
    Straight key fix

---
+++ src/RebelAllianceMod_v2_3/TT506.h
@@ -0,0 +1,45 @@
+// various defines and UNO32 pin layout
+#define SDATA_BIT                           10          //  keep!
+#define SCLK_BIT                            8           //  keep!
+#define FSYNC_BIT                           9           //  keep!
+#define RESET_BIT                           11          //  keep!
+#define FREQ_REGISTER_BIT                   12          //  keep!
+#define AD9834_FREQ0_REGISTER_SELECT_BIT    0x4000      //  keep!
+#define AD9834_FREQ1_REGISTER_SELECT_BIT    0x8000      //  keep!
+#define FREQ0_INIT_VALUE                    0x01320000  //  ?
+
+#define led                                 13          // Ten*Ten led
+#define Side_Tone                           3           // maybe to be changed to a logic control
+                                                        // for a separate side tone gen
+#define TX_Dah                              33          //  keep!
+#define TX_Dit                              32          //  keep!
+#define TX_OUT                              38          //  keep!
+
+#define Band_End_Flash_led                  24          // also this led will flash every 100/1khz/10khz is tuned
+#define Band_Select                         41          // if shorting block on only one pin 20m(1) on both pins 40m(0)
+#define Multi_Function_Button               2           //
+#define Multi_function_Green                34          // For now assigned to BW (Band width)
+#define Multi_function_Yellow               35          // For now assigned to STEP size
+#define Multi_function_Red                  36          // For now assigned to USER
+
+#define Select_Button                       5           // 
+#define Select_Green                        37          // Wide/100/USER1
+#define Select_Yellow                       39          // Medium/1K/USER2
+#define Select_Red                          40          // Narrow/10K/USER3
+
+#define Medium_A8                           22          // Hardware control of I.F. filter Bandwidth
+#define Narrow_A9                           23          // Hardware control of I.F. filter Bandwidth
+
+#define Wide_BW                             0           // About 2.1 KHZ
+#define Medium_BW                           1           // About 1.7 KHZ
+#define Narrow_BW                           2           // About 1 KHZ
+
+#define Step_100_Hz                         0
+#define Step_1000_hz                        1
+#define Step_10000_hz                       2
+
+#define  Other_1_user                       0           // 
+#define  Other_2_user                       1           //
+#define  Other_3_user                       2           //
+
+
